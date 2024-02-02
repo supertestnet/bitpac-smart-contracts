3e3020c6799d0c0b4feec07b60d4b14d83b447f3
supertestnet <58400631+supertestnet@users.noreply.github.com>
2024-02-02 12:02:53 -0600

    Update contracts.js

--- contracts.js
+++ contracts.js
@@ -1,3 +1,1 @@
-{
-    "6bc1c3c6c7d7f4473f9b794d8b7adb057f480580c452acb2f9f930ba3fa6e152": "(async()=>{var data = await fetch( 'https://api.coinbase.com/v2/prices/BTC-USD/spot' );var json = await data.json();var price = Number( json[ 'data' ][ 'amount' ] );if ( price >= contract.target_price ) sendTo( contract.longer );else sendTo( contract.shorter );})();"
-}
+{"6bc1c3c6c7d7f4473f9b794d8b7adb057f480580c452acb2f9f930ba3fa6e152": "(async()=>{var data = await fetch( 'https://api.coinbase.com/v2/prices/BTC-USD/spot' );var json = await data.json();var price = Number( json[ 'data' ][ 'amount' ] );if ( price >= contract.target_price ) sendTo( contract.longer );else sendTo( contract.shorter );})();"}
